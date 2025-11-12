36102bbf16c1b6d6a4e9b1fe68de7a9cae9337f0
haimamire <ivanmatusevich16@gmail.com>
2025-11-12 09:27:01 +0900

    Remove map limits, adjust top bar & more

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -1,6 +1,7 @@
 import '../styles/styles.css';
 import '../styles/map.css';
 import '../styles/mapstyles.css';
+import 'leaflet.markercluster';
 
 import { northLimit, southLimit, eastLimit, westLimit } from './mapUtilities/mapData';
 
@@ -11,7 +12,7 @@ import { initGPS } from './gpsRealTime';
 
 document.addEventListener('DOMContentLoaded', () => {
   const config = {
-    minZoom: 13,
+    // minZoom: 11,
     maxZoom: 19,
     zoomControl: false,
     zoomSnap: 0.5,
@@ -23,16 +24,24 @@ document.addEventListener('DOMContentLoaded', () => {
     [34.98493616431302, 135.75248977767515],
     initialZoom,
   );
-  MAP.setMaxBounds([
-    [northLimit, eastLimit],
-    [southLimit, westLimit],
-  ]);
+  // MAP.setMaxBounds([
+  //   [northLimit, eastLimit],
+  //   [southLimit, westLimit],
+  // ]);
 
   // This adds all of the lines we are gonna use for the routes and cycle lanes
   addLayers(MAP);
   addPolyline(MAP);
   addMarkers(MAP);
   initGPS(MAP);
+
+  const cluster = window.L.markerClusterGroup({
+    iconCreateFunction: function (cluster) {
+      return L.divIcon({
+        html: '<div></div>',
+      });
+    }
+  });
 
   // Loop through all images on the page adding necessary styles and 
   // event listener to prevent images from opening the context menu.
