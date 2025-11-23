8f48ceae6a96a00eb6d0d1c47145263be4abeffd
Yocker <mdyoc74@gmail.com>
2025-11-23 20:19:30 +0900

    Add Unauthenticated Page file and styles

---
+++ src/scripts/frontendCommunity/pages/UnauthenticatedPage.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+// import Login from '../components/Login';
+// import SignUp from '../components/SignUp';
+import '../styles/UnauthenticatedPage.css';
+
+const UnauthenticatedPage = () => {
+  const [isLogin, setIsLogin] = useState(true);
+
+  const toggleForm = () => {
+    setIsLogin((prevIsLogin) => !prevIsLogin);
+  };
+
+  return (
+    <div className="unauthenticated-container">
+      <div className="unauthenticated-content">
+        {/* Welcome Section */}
+        <div className="welcome-section">
+          <h2 className="welcome-title">
+            Welcome to <span className="accent-text">Flex</span>or
+          </h2>
+        </div>
+
+        {/* Form Section */}
+        <div className="form-section">
+          {/* Conditional Form Rendering */}
+          {isLogin ? (
+            <>
+              <Login />
+              <div className='divider'></div>
+              <button
+                onClick={toggleForm}
+                className="toggle-button"
+              >
+                Don't have an account? Sign Up
+              </button>
+            </>
+          ) : (
+            <>
+              <SignUp />
+              <div className='divider'></div>
+              <button
+                onClick={toggleForm}
+                className="toggle-button"
+              >
+                Already have an account? Log In
+              </button>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UnauthenticatedPage;
